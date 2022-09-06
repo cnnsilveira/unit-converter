7c0dd4431eb80ab824317f792ca1cf354acb65f6
Caio Nunes <caionunessteam@hotmail.com>
2022-09-06 07:08:54 -0300

    adding some conditionals

--- index.js
+++ index.js
@@ -5,14 +5,22 @@ const volume = document.querySelector("#volume-el")
 const mass = document.querySelector("#mass-el")
 
 btn.addEventListener("click", function() {
-    const inFeet = input.value * 3.281
-    const inMeter = input.value / 3.281
-    const inGallon = input.value * 0.264
-    const inLiter = input.value / 0.264
-    const inPound = input.value * 2.204
-    const inKilo = input.value / 2.204
-    length.textContent = `${input.value} meters = ${inFeet.toFixed(3)} feet | ${input.value} feet = ${inMeter.toFixed(3)} meters`
-    volume.textContent = `${input.value} liters = ${inGallon.toFixed(3)} gallons | ${input.value} gallons = ${inLiter.toFixed(3)} liters`  
-    mass.textContent = `${input.value} kilos = ${inPound.toFixed(3)} pounds | ${input.value} pounds = ${inKilo.toFixed(3)} kilos`   
-    input.value = ""
+    if (input.value === "") {
+        alert("Please inform a value.")
+        input.value = ""
+    } else if (isNaN(input.value)) {
+        alert("Informed value must be a number.")
+        input.value = ""
+    } else {
+        const inFeet = input.value * 3.281
+        const inMeter = input.value / 3.281
+        const inGallon = input.value * 0.264
+        const inLiter = input.value / 0.264
+        const inPound = input.value * 2.204
+        const inKilo = input.value / 2.204
+        length.textContent = `${input.value} meters = ${inFeet.toFixed(3)} feet | ${input.value} feet = ${inMeter.toFixed(3)} meters`
+        volume.textContent = `${input.value} liters = ${inGallon.toFixed(3)} gallons | ${input.value} gallons = ${inLiter.toFixed(3)} liters`  
+        mass.textContent = `${input.value} kilos = ${inPound.toFixed(3)} pounds | ${input.value} pounds = ${inKilo.toFixed(3)} kilos`   
+        input.value = ""
+    }
 })
